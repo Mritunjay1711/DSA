53c1e9981c0d81d759b120202269c01dc14d2fb6
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-07-06 23:13:12 +0530

    new

---
+++ Stack/StackOperations.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr){
+    if(ptr->top == -1)
+        return 1;
+    else return 0;
+}
+
+int isFull(struct stack *ptr){
+    if(ptr->top == ptr->size - 1)
+        return 1;
+    else return 0;
+}
+
+void push(struct stack *s,int data)
+{
+    if(!isFull(s))
+    {
+        s->top++;
+        s->arr[s->top] = data;
+    }
+    else
+        printf("Stack Overflow!, Cannot push %d in Stack.\n", data);
+    
+}
+
+int pop(struct stack *s)
+{
+    if(!isEmpty(s))
+    {
+        int val = s->arr[s->top];
+        s->top--;
+        return val;
+    }
+    else
+    {
+        printf("Stack Underflow.\n");
+        return -1;
+    }
+       
+}
+
+int main()
+{
+    struct stack *s = (struct stack*)malloc(sizeof(struct stack));
+    s->size = 6;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int)); 
+
+    printf("Before pushing, Full: %d\n", isFull(s));
+    printf("Before pushing, Empty: %d\n", isEmpty(s));
+    push(s, 55);
+    push(s, 52);
+    push(s, 75);
+    push(s, 56);
+    push(s, 45);
+    push(s, 2);
+    push(s, 34); //Overflow
+    printf("After pushing, Full: %d\n", isFull(s));
+    printf("After pushing, Empty: %d\n", isEmpty(s));
+
+    printf("Popped %d from the stack.\n", pop(s));
+    printf("Popped %d from the stack.\n", pop(s));
+    
+    return 0;
+}
