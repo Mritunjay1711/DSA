01e16edffb735b318707f83c7d18635809552128
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-07-07 16:06:47 +0530

    new

---
+++ Stack/stackTopAndstackBottom.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isFull(struct stack *s)
+{
+    return (s->top == s->size -1) ? 1 : 0;
+}
+
+int isEmpty(struct stack *s)
+{
+    return (s->top == -1) ? 1 : 0;
+}
+
+void push(struct stack *s, int data)
+{
+    if(isFull(s))
+    {
+        printf("Stack Overflow!\n");
+    }
+    else
+    {
+        s->top++;
+        s->arr[s->top] = data;
+    }
+}
+
+int pop(struct stack *s)
+{
+    if(isEmpty(s))
+    {
+        printf("Stack Underflow!\n");
+        return -1;
+    }
+    else
+    {
+        return s->arr[s->top--];
+    }
+}
+
+int peek(struct stack *s, int index)
+{
+    int arrInd = s->top - index + 1;
+    if(arrInd < 0)
+    {
+        printf("Invalid Index!\n");
+        return -1;
+    }
+    else
+    {
+        return s->arr[arrInd];
+    }
+}
+
+int stackTop(struct stack *s)
+{
+    return s->arr[s->top];
+}
+
+int stackBottom(struct stack *s)
+{
+    return s->arr[0];
+}
+
+int main()
+{
+    struct stack *s = (struct stack*)malloc(sizeof(struct stack));
+    s->size = 10;
+    s->top = -1;
+    s->arr = (int*)malloc(s->size * sizeof(int));
+
+    for(int i = 0; i < 4; i++)
+    {
+        int temp;
+        printf("Enter the data to push into Stack-> ");
+        scanf("%d", &temp);
+        push(s, temp);
+    }
+    for(int i = 1; i <= s->top + 1; i++)
+    {
+        printf("The value at index %d in Stack is %d.\n", i, peek(s, i));
+    }
+    printf("\n");
+    printf("The Value at top of stack is %d.\n", stackTop(s));
+    printf("The value at bottom of the stack is %d.\n", stackBottom(s));
+    return 0;
+}
