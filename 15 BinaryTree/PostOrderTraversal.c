03ededcc8ee6fc7ed984afda87cb2ad056795f30
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-07-23 00:19:15 +0530

    new

---
+++ 15 BinaryTree/PostOrderTraversal.c
@@ -0,0 +1,60 @@
+// Left-->Right-->Root
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n = (struct node*)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL; 
+    n->right = NULL;
+
+    return n;
+}
+
+void PostOrder(struct node *root)
+{
+    if(root != NULL)
+    {
+        PostOrder(root->left);
+        PostOrder(root->right);
+        printf("%d ",root->data);
+    }
+}
+
+int main()
+{
+    // Constructing the root node
+    struct node *p = createNode(4);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+
+
+    /*Tree
+            4
+           / \
+          1   6
+         / \
+        5   2
+    */
+
+   
+    // Linking
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    
+
+    PostOrder(p);
+    printf("\n");
+    return 0;
+}
