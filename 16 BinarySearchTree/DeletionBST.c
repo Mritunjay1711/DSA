587e0311112882b7385c438d324d1be06eff7ca6
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-07-25 21:45:41 +0530

    new

---
+++ 16 BinarySearchTree/DeletionBST.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+
+    return n;
+}
+
+void InOrderTraversal(struct node *root)
+{
+    if (root != NULL)
+    {
+        InOrderTraversal(root->left);
+        printf("%d ", root->data);
+        InOrderTraversal(root->right);
+    }
+}
+
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+        return 1;
+}
+
+struct node *inOrderPredecessor(struct node *root)
+{
+    root = root->left;
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct node *deleteNode(struct node *root, int value)
+{
+    struct node *iPre;
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        free(root);
+        return NULL;
+    }
+
+    // Search for the node to be deleted
+    if (value < root->data)
+    {
+        root->left = deleteNode(root->left, value);
+    }
+    else if (value > root->data)
+    {
+        root->right = deleteNode(root->right, value);
+    }
+    // Deletion strategy when the node is found
+    else
+    {
+        iPre = inOrderPredecessor(root);
+        root->data = iPre->data;
+        root->left = deleteNode(root->left, iPre->data);
+    }
+    return root;
+}
+
+int main()
+{
+    /*
+            8
+          /   \
+        5       10
+      /  \     /  \
+    3     6   9   17
+    
+    */
+
+    struct node *p = createNode(8);
+    struct node *p1 = createNode(5);
+    struct node *p2 = createNode(10);
+    struct node *p3 = createNode(3);
+    struct node *p4 = createNode(6);
+    struct node *p5 = createNode(9);
+    struct node *p6 = createNode(17);
+
+    // Linking
+    p->left = p1;
+    p->right = p2;
+
+    p1->left = p3;
+    p1->right = p4;
+
+    p2->left = p5;
+    p2->right = p6;
+
+    InOrderTraversal(p);
+    printf("\n%d\n", isBST(p));
+    p = deleteNode(p, 8);
+    InOrderTraversal(p);
+    printf("\n%d is root now!\n", p->data);
+
+    return 0;
+}
