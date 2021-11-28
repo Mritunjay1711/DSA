698bae81727fab2167129e9f1b670169eadf4e7d
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-11-28 22:41:30 +0530

    new

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,26 +1,9 @@
 #include<iostream>
 using namespace std;
 
-class sample
-{
-public:
-    void set_data(char c)
-    {
-        ch = c;
-    }
-    void show_data()
-    {
-        cout << a << " " << ch;
-    }
-private:
-    int a = 10;
-    char ch;
-};
-
-
 int main(){
-    sample s;
-    s.set_data('d');
-    s.show_data();    
+    cout << "\n Hello- cout";
+    cerr << "\n Hello World- cerr";
+    clog << "\n Hello C++ - clog";  
     return 0;
 }
